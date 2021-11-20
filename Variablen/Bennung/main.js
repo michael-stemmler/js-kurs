39288b7df7243738ee307228b29d2f5258092924
Michael Stemmler <github@mail.sthgw.de>
2021-11-20 20:29:49 +0100

    .

---
+++ Variablen/Bennung/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let variable = "Meine Variable"
+// Kommentar
+
+//let name! = "Mein Name"
+
+//let 9name= "Name";
+
+let name = "Name-Klein";
+let Name = "Name-Groß";
+
+console.log(name);
+console.log(Name);
+
+let hängematte = "bla";
+console.log(hängematte)
+
+let mein_name = "Klaus";
+console.log(mein_name)
+
+//let let = "falsch";
+//console.log(let);
---
+++ Variablen/Variablen in JS/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let stadt = "Greifswald";
+let alter = 24;
+let test;
+
+console.log(stadt);
+console.log(alter);
+console.log(test);
